f103eac0e3b3c9e0f17128cd19e63f78b6985cc2
David Dunson <david.dunson@i3-corps.com>
2023-08-22 09:15:02 -0500

    Add project files.

---
+++ Sample.DownloadService/WindowsBackgroundService.cs
@@ -0,0 +1,46 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WindowsBackgroundService.cs" company="United States Government">
+//   © 2022 United States Government, as represented by the Secretary of the Army.  All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Sample.DownloadService;
+
+public sealed class WindowsBackgroundService : BackgroundService
+{
+    private readonly JokeService _jokeService;
+    private readonly ILogger<WindowsBackgroundService> _logger;
+
+    public WindowsBackgroundService(
+        JokeService jokeService,
+        ILogger<WindowsBackgroundService> logger) =>
+        (this._jokeService, this._logger) = (jokeService, logger);
+
+    // Example from: https://docs.microsoft.com/en-us/dotnet/core/extensions/windows-service
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Sample", "DownloadService", "Runtime");
+            var path = Path.Combine(directory, "download.log");
+            string joke = this._jokeService.GetJoke();
+
+            try
+            {
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+
+                await File.WriteAllTextAsync(path, joke, stoppingToken);
+                this._logger.LogWarning($"Successfully updated {path}");
+            }
+            catch (Exception ex)
+            {
+                this._logger.LogError(ex, $"Unable to write to {path}");
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+        }
+    }
+}
